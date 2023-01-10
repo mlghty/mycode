48095f0458eb16b8e53922cba76858cfcb98f23e
mlghty <vvarfare@gmail.com>
2023-01-10 16:36:04 +0000

    join and split

---
+++ input.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+
+user_input_name = input("What is your username")
+
+
+user_input_day_of_week = input("What day is it? ")
+
+print("Hello, " , user_input_name , "!", " Happy ", user_input_day_of_week, "!")
+
