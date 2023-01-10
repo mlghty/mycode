02fe44cc703fe3848094c50ac852e31d1ed2a5c0
mlghty <vvarfare@gmail.com>
2023-01-10 17:27:40 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# create a list containing three items
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# display the first item in the list
+print("The first ip is: " + iplist[3]+ " and the second is " + iplist[4])
